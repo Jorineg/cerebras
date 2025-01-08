e4e3d4246246956d7ed0505a11062bd77d09233c
Jorineg <jorineggers@gmail.com>
2025-01-08 16:11:45 +0100

    reduce to 3x3 grid and simplify by removing n/s directions

--- r1-stencil/run.sh
+++ r1-stencil/run.sh
@@ -1,3 +1,3 @@
 export SINGULARITYENV_SIMFABRIC_DEBUG=inst_trace
-cslc layout.csl --fabric-dims=13,8 --fabric-offsets=4,1 --params=w:6,h:6,steps:1 --memcpy --channels=1 --arch=wse2 -o out
+cslc layout.csl --fabric-dims=10,5 --fabric-offsets=4,1 --params=w:3,h:3,steps:1 --memcpy --channels=1 --arch=wse2 -o out
 cs_python run.py --name out
