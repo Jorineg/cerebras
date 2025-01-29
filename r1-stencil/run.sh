91cebff9401e238166565d5751b118aa3b4807a9
Jorineg <jorineggers@gmail.com>
2025-01-29 15:47:37 +0100

    works with up to four steps. kernel stall for 5+ steps. not sure why...

--- r1-stencil/run.sh
+++ r1-stencil/run.sh
@@ -1,3 +1,3 @@
 export SINGULARITYENV_SIMFABRIC_DEBUG=inst_trace
-cslc layout.csl --fabric-dims=14,8 --fabric-offsets=4,1 --params=w:6,h:5,steps:1 --memcpy --channels=1 --arch=wse2 -o out
+cslc layout.csl --fabric-dims=14,8 --fabric-offsets=4,1 --params=w:6,h:5,steps:5 --memcpy --channels=1 --arch=wse2 -o out
 cs_python run.py --name out
