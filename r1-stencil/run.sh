5bf1137c6df969f07386687e9139571f17fa9b2f
Jorineg <jorineggers@gmail.com>
2025-02-05 15:22:34 +0100

    use dsr for send. 36 cycles

--- r1-stencil/run.sh
+++ r1-stencil/run.sh
@@ -1,3 +1,3 @@
 export SINGULARITYENV_SIMFABRIC_DEBUG=inst_trace
-cslc layout.csl --fabric-dims=12,7 --fabric-offsets=4,1 --params=w:4,h:3,steps:100 --memcpy --channels=1 --arch=wse2 -o out
+cslc layout.csl --fabric-dims=12,7 --fabric-offsets=4,1 --params=w:4,h:3,steps:10 --memcpy --channels=1 --arch=wse2 -o out
 cs_python run.py --name out
