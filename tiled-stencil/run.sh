e5ca268fa18d535d59d7ca0a7b25cda618d16441
Jorineg <jorineggers@gmail.com>
2025-06-04 15:13:36 +0200

    use fmacs

--- tiled-stencil/run.sh
+++ tiled-stencil/run.sh
@@ -1,3 +1,3 @@
 export SINGULARITYENV_SIMFABRIC_DEBUG=landing
-cslc layout.csl --fabric-dims=16,12 --fabric-offsets=4,1 --params=w:14,h:14,tile_width:6,tile_height:6,radius:1,num_iterations:2 --memcpy --channels=1 --arch=wse2 -o out
+cslc layout.csl --fabric-dims=16,12 --fabric-offsets=4,1 --params=w:14,h:14,tile_width:6,tile_height:6,radius:1,num_iterations:1 --memcpy --channels=1 --arch=wse2 -o out
 cs_python run.py --name out
