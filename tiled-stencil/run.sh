68085cbd3537ce5f3aa9c1f9d039db12c26e41e3
Jorineg <jorineggers@gmail.com>
2025-06-13 18:56:01 +0200

    code restructuring

--- tiled-stencil/run.sh
+++ tiled-stencil/run.sh
@@ -1,11 +1,58 @@
 export SINGULARITYENV_SIMFABRIC_DEBUG=inst_trace
+
+WIDTH=14
+HEIGHT=14
+TILE_WIDTH=6
+TILE_HEIGHT=6
+RADIUS=1
+NUM_ITERATIONS=2
+ARCH="wse2"
+
+# increase width and height so they are multiples of tile_width and tile_height
+INNER_W=$((WIDTH - 2 * RADIUS))
+INNER_H=$((HEIGHT - 2 * RADIUS))
+
+REMAINDER_W=$((INNER_W % TILE_WIDTH))
+REMAINDER_H=$((INNER_H % TILE_HEIGHT))
+
+if [ $REMAINDER_W -ne 0 ]; then
+  PAD_W=$((TILE_WIDTH - REMAINDER_W))
+  echo "increasing width from $WIDTH to $((WIDTH + PAD_W))"
+  WIDTH=$((WIDTH + PAD_W))
+fi
+
+if [ $REMAINDER_H -ne 0 ]; then
+  PAD_H=$((TILE_HEIGHT - REMAINDER_H))
+  echo "increasing height from $HEIGHT to $((HEIGHT + PAD_H))"
+  HEIGHT=$((HEIGHT + PAD_H))
+fi
+
+NUM_PE_X=$((INNER_W / TILE_WIDTH + 2))
+NUM_PE_Y=$((INNER_H / TILE_HEIGHT + 2))
+
+
 cslc layout.csl \
---fabric-dims=16,12 \
+--fabric-dims=$((NUM_PE_X+7)),$((NUM_PE_Y+2)) \
 --fabric-offsets=4,1 \
---params=w:14,h:14,tile_width:6,tile_height:6,radius:1,num_iterations:10 \
+--params=w:$WIDTH,h:$HEIGHT,tile_width:$TILE_WIDTH,tile_height:$TILE_HEIGHT,radius:$RADIUS,num_iterations:$NUM_ITERATIONS \
 --memcpy \
 --channels=1 \
---arch=wse2 \
---link-section-start-address-bytes=".buffer1:20960,.buffer2:40960" \
+--arch=$ARCH \
+--link-section-start-address-bytes=".own_values:20960,.buffer:40960" \
 -o out
-cs_python run.py --name out
+if [ $? -ne 0 ]; then
+  echo "cslc failed" >&2
+  exit 1
+fi
+cs_python run.py --name out >> python.log
+if [ $? -ne 0 ]; then
+    echo "cerebras computation doesn't match python computation" >&2
+else
+    echo "Success!"
+fi
+python3 filter_traces.py <<EOF
+5
+2
+EOF
+echo "Total Cerebras cycles:"
+python3 get_cycles.py
